52e7f61ac58a1935e2abba416b44a441617bb025
JamieW105 <jamie.wells@stpats.school.nz>
2025-06-27 17:11:10 +1200

    website

--- vc-radio-app/src/App.tsx
+++ vc-radio-app/src/App.tsx
@@ -2,22 +2,5 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return (
-    <div className="main-bg">
-      <header className="header">
-        <h1>Nexus-Radio</h1>
-        <p className="subtitle">Live VC Dispatch & Radio Integration</p>
-      </header>
-      <div className="actions">
-        <button className="login-btn">Login with Discord</button>
-        <button className="dispatch-btn">Dispatch Unit</button>
-        <button className="log-btn">View Call Log</button>
-      </div>
-      <div className="radio-visualizer">
-        {[...Array(10)].map((_, i) => (
-          <div key={i} className="bar" style={{ animationDelay: `${i * 0.1}s` }} />
-        ))}
-      </div>
-    </div>
-  );
+  return <div className="main-bg"></div>;
 }
